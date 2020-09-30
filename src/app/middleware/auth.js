d6635cb9a901acd7c1edc5c09785b87f5260e101
guistetter <guilherme_stetter@hotmail.com>
2020-09-30 17:44:42 -0300

    verificacao de usuario

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,5 +1,25 @@
-export default (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
-  return next();
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // verificar e validar token do usuario, id do usuario vem no decoded pois estava no payload
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'token invalid' });
+  }
 };
