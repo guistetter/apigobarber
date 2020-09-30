ea49b206469aac7649f2a1c6daf30866286cdcca
guistetter <guilherme_stetter@hotmail.com>
2020-09-30 17:12:36 -0300

    refatorando configs de autenticacao

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
@@ -24,8 +25,8 @@ class SessionController {
         email,
       },
       // payload do usuario
-      token: jwt.sign({ id }, 'ac691adcb63199049b9345b4a1cbadad', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
