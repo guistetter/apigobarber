d99d95410fbb055dd82ab249abb8dfac354ce7b5
guistetter <guilherme_stetter@hotmail.com>
2020-09-30 14:59:15 -0300

    credenciais database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'goStack',
+  database: 'goStack',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
