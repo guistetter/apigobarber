5bb8ff5a55a9a71054a6f541e3389e163b63e694
guistetter <guilherme_stetter@hotmail.com>
2020-10-02 16:15:13 -0300

    controller p/ notification do prestador-servico

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,26 @@
+import Notification from '../Schemas/Notification';
+import User from '../models/User';
+
+class Notificationcontroller {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      // userId vem automatico pelo metodo auth
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notification' });
+    }
+
+    const notification = await Notification.find({
+      user: req.userId,
+    })
+      .sort('createdAt')
+      .limit(20);
+
+    return res.json(notification);
+  }
+}
+export default new Notificationcontroller();
