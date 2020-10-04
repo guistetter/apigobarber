23a359846129cbd69f0e501d7c6f41870778dcf1
guistetter <guilherme_stetter@hotmail.com>
2020-10-04 14:52:09 -0300

    fix configurations sentry

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from 'path';
 import * as Sentry from '@sentry/node';
-import routes from './routes';
+import * as Tracing from '@sentry/tracing';
 import sentryConfig from './config/sentry';
+import routes from './routes';
+
 import 'express-async-errors';
 
 import './database';
@@ -10,13 +12,26 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-    Sentry.init(sentryConfig);
+
+    Sentry.init(sentryConfig, {
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express(this.server),
+      ],
+
+      tracesSampleRate: 1.0,
+    });
+
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(Sentry.Handlers.tracingHandler());
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,4 @@
 export default {
-  dns:
-    'https://2063efcd5a3b4939b6dd433d0add59a9@o400080.ingest.sentry.io/5450380',
+  dsn:
+    'https://71121c2ef1044a479bba19360e7bd835@o400080.ingest.sentry.io/5450398',
 };
