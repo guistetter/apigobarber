792c2574305ecfac65ca246cb69093cbb691cbd5
guistetter <guilherme_stetter@hotmail.com>
2020-10-04 15:08:41 -0300

    tratando erros

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
-import sentryConfig from './config/sentry';
-import routes from './routes';
-
 import 'express-async-errors';
+
+import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -29,6 +30,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandle();
   }
 
   middlewares() {
@@ -42,6 +44,13 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandle() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
