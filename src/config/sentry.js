6dde3a771061d0c0d61b82caee317413239c3e72
guistetter <guilherme_stetter@hotmail.com>
2020-10-04 15:35:13 -0300

    refatoran configurando variaveis ambiente

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-  dsn:
-    'https://71121c2ef1044a479bba19360e7bd835@o400080.ingest.sentry.io/5450398',
+  dsn: process.env.SENTRY_DSN,
 };
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,6 @@
 // Com está configuracao rodamos a fila em outro 'node'
 // abro outro terminal e rodo node/src queue.js
+import 'dotenv/config';
 import Queue from './lib/Queue';
 
 Queue.processQueue();
